492645310a9e1a00e74db33d4287542452b68528
liu-junyong <720556@qq.com>
2017-12-06 15:21:06 +0800

    init

--- reflectehelper.go
+++ reflectehelper.go
@@ -83,3 +83,40 @@ func Struct2Map(obj interface{}) map[string]interface{} {
     }
     return data
 }
+
+
+//类型转换
+func TypeConversion(value string, ntype string) (reflect.Value, error) {
+    if ntype == "string" {
+        return reflect.ValueOf(value), nil
+    } else if ntype == "time.Time" {
+        t, err := time.ParseInLocation("2006-01-02 15:04:05", value, time.Local)
+        return reflect.ValueOf(t), err
+    } else if ntype == "Time" {
+        t, err := time.ParseInLocation("2006-01-02 15:04:05", value, time.Local)
+        return reflect.ValueOf(t), err
+    } else if ntype == "int" {
+        i, err := strconv.Atoi(value)
+        return reflect.ValueOf(i), err
+    } else if ntype == "int8" {
+        i, err := strconv.ParseInt(value, 10, 64)
+        return reflect.ValueOf(int8(i)), err
+    } else if ntype == "int32" {
+        i, err := strconv.ParseInt(value, 10, 64)
+        return reflect.ValueOf(int64(i)), err
+    } else if ntype == "int64" {
+        i, err := strconv.ParseInt(value, 10, 64)
+        return reflect.ValueOf(i), err
+    } else if ntype == "float32" {
+        i, err := strconv.ParseFloat(value, 64)
+        return reflect.ValueOf(float32(i)), err
+    } else if ntype == "float64" {
+        i, err := strconv.ParseFloat(value, 64)
+        return reflect.ValueOf(i), err
+    }
+
+    //else if .......增加其他一些类型的转换
+
+    return reflect.ValueOf(value), errors.New("未知的类型：" + ntype)
+}
+
